eb73b28726bb7c1332eaf2d88f4bf4e30b0fadb4
Your Name <you@example.com>
2025-04-22 21:52:11 +0300

    fix typo

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -457,7 +457,7 @@ const SignUp = () => {
               Verified
             </Text>
             <Text className="text-base text-gray-400 font-Jakarta text-center mt-2">
-              You have successfully verified your account.
+              Успешно влязохте с профила си в Google
             </Text>
             <CustomButton
               title="Към началната страница"
